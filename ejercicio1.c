4f982f939be45a3bfd684928fe3574fe80a330d6
Quielo Gogez <67394751+Quielo@users.noreply.github.com>
2021-08-01 14:59:32 -0500

    Homework

---
+++ ejercicio1.c
@@ -0,0 +1,16 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<sys/wait.h>
+
+int main()
+{
+	int id;
+	if(id = 0)
+	{
+		printf("%s %d\n","Soy el hijo y mi id: ", getpid());
+		sleep(10);
+	}else{
+		wait(NULL);
+		printf("%s %d\n", "Soy el padre y mi id: ", getpid());
+	}
+}
